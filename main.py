4d73772260776a53c635ae7dbeadb518ef5912b6
Michael Primavera <pastasauce59@gmail.com>
2023-05-29 23:54:28 -0400

    Sent generated letter successfully to person's email using smtplib and starting a connection.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import datetime as dt
 import pandas
 import random
+import smtplib
+from keys import *
 
 now = dt.datetime.now()
 month = now.date().month
@@ -16,4 +18,13 @@ bday_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in d
 if today_tuple in bday_dict:
     with open(f"letter_templates/letter_{random.randint(1,3)}.txt") as letter:
         new_letter = letter.read()
-        print(new_letter.replace("[NAME]", bday_dict[today_tuple]['name']))
+        # print(new_letter.replace("[NAME]", bday_dict[today_tuple]['name']))
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_sender,
+            msg=f"Subject: HAPPY BIRTHDAY!\n\n{new_letter.replace('[NAME]', bday_dict[today_tuple]['name'])}"
+            )
